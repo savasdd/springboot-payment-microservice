d942cab9fa0a02f3379137e58cb96a009e684279
savasdede <svsdd22@gmail.com>
2024-07-27 22:16:41 +0300

    develop user ExceptionHandler

---
+++ microservice-user/common/src/main/java/com/payment/user/common/exception/ExceptionResponse.java
@@ -0,0 +1,49 @@
+package com.payment.user.common.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ExceptionResponse {
+    private static MessageSource messageSource;
+
+    private Date time;
+    private int status;
+    private Object error;
+    private Object message;
+    private String path;
+
+    public ExceptionResponse(String infoMessage) {
+        this.message = getLangMessage(infoMessage);
+    }
+
+    public static void SetMessageSource(MessageSource ms) {
+        messageSource = ms;
+    }
+
+    public ExceptionResponse() {
+    }
+
+    public ExceptionResponse(Date time, int status, Object error, Object message, String path) {
+        this.time = time;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    private String getLangMessage(String msg) {
+        String message = StringUtils.substringBetween(msg, "{", "}");
+        if (message == null) {
+            return msg;
+        }
+        String anotherMessage = msg.replace("{" + message + "}", "");
+        return messageSource.getMessage(message, null, LocaleContextHolder.getLocale()) + "\n" + ((!anotherMessage.isEmpty()) ? anotherMessage : "");
+    }
+}
---
+++ microservice-user/common/src/main/java/com/payment/user/common/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package com.payment.user.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.Serial;
+
+@Slf4j
+public class GeneralException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = -2803799245666892878L;
+
+    public GeneralException(String errorMessage) {
+        super(errorMessage);
+        log.error(errorMessage);
+    }
+
+    public GeneralException() {
+        super();
+    }
+}
