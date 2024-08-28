e98d24419369d8afbd2ac7a37ae38be7ed2280b5
savasdede <svsdd22@gmail.com>
2024-08-29 01:59:51 +0300

    develop user prod config

--- microservice-user/application/src/main/java/com/payment/user/application/UserApplication.java
+++ microservice-user/application/src/main/java/com/payment/user/application/UserApplication.java
@@ -3,6 +3,7 @@ package com.payment.user.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 
@@ -10,6 +11,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 @SpringBootApplication(scanBasePackages = "com.payment.user")
 @EntityScan(basePackages = "com.payment.user")
 @EnableJpaRepositories("com.payment.user")
+@PropertySource({"file:/Users/savasdede/Desktop/svsdd/Conf/application_user.yml"})
 public class UserApplication {
 
     public static void main(String[] args) {
