ee1f56768baf98f047d47917a8024c1fdfaf39aa
savasdede <svsdd22@gmail.com>
2024-09-02 21:59:24 +0300

    discovery fix

--- microservice-discovery/application/src/main/java/com/payment/discovery/application/DiscoveryApplication.java
+++ microservice-discovery/application/src/main/java/com/payment/discovery/application/DiscoveryApplication.java
@@ -2,8 +2,12 @@ package com.payment.discovery.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaServer
+@RestController
 public class DiscoveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class, args);
