ac73ac3f2e0833bc3ddbc1f9ae021506fd2cbb14
savasdede <svsdd22@gmail.com>
2024-09-03 15:22:40 +0300

    develop user fix

--- microservice-user/application/src/main/java/com/payment/user/application/UserApplication.java
+++ microservice-user/application/src/main/java/com/payment/user/application/UserApplication.java
@@ -3,10 +3,12 @@ package com.payment.user.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableEurekaClient
 @EnableKafka
 @SpringBootApplication(scanBasePackages = "com.payment.user")
 @EntityScan(basePackages = "com.payment.user")
