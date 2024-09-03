9906d125f86a0a641be52c7863bb8ff274bd1953
savasdede <svsdd22@gmail.com>
2024-09-03 15:27:18 +0300

    stock development to config kafka

--- microservice-stok/application/src/main/java/com/payment/stock/application/StockApplication.java
+++ microservice-stok/application/src/main/java/com/payment/stock/application/StockApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.payment.stock")
 @EntityScan(basePackages = "com.payment.stock")
 @EnableJpaRepositories(basePackages = "com.payment.stock")
