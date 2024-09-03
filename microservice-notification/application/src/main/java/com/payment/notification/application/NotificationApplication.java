790e8c3f1578634d23a8273e98d63fa85d863574
savasdede <svsdd22@gmail.com>
2024-09-03 15:32:52 +0300

    development notification fix

--- microservice-notification/application/src/main/java/com/payment/notification/application/NotificationApplication.java
+++ microservice-notification/application/src/main/java/com/payment/notification/application/NotificationApplication.java
@@ -2,10 +2,12 @@ package com.payment.notification.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.kafka.annotation.EnableKafka;
 
-@SpringBootApplication(scanBasePackages = "com.payment.notification")
 @EnableKafka
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages = "com.payment.notification")
 public class NotificationApplication {
 
     public static void main(String[] args) {
