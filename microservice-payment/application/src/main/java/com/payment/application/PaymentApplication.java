2dbc9d6751fbcb662f9d14391172391a9ed45356
savasdede <svsdd22@gmail.com>
2024-09-03 15:39:10 +0300

    payment development fix

--- microservice-payment/application/src/main/java/com/payment/application/PaymentApplication.java
+++ microservice-payment/application/src/main/java/com/payment/application/PaymentApplication.java
@@ -4,17 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableKafka
+@EnableScheduling
+@EnableEurekaClient
+@EnableCaching(mode = AdviceMode.ASPECTJ)
 @SpringBootApplication(scanBasePackages = "com.payment")
 @EntityScan(basePackages = "com.payment")
 @EnableJpaRepositories(basePackages = "com.payment")
-@EnableKafka
-@EnableScheduling
-@EnableCaching(mode = AdviceMode.ASPECTJ)
 public class PaymentApplication {
 
     public static void main(String[] args) {
