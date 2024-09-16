6c539ed7e12ac7c6d2b68801217d750b890d55bb
savasdede <svsdd22@gmail.com>
2024-09-16 09:32:37 +0300

    report fix

--- microservice-report/application/src/main/java/com/payment/report/application/ReportApplication.java
+++ microservice-report/application/src/main/java/com/payment/report/application/ReportApplication.java
@@ -2,7 +2,9 @@ package com.payment.report.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.payment.report")
 public class ReportApplication {
 
