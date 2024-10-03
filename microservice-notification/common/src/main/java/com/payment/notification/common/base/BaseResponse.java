6310c6cee9367f1b5272067caba14f8c88a0c9f0
savasdede <svsdd22@gmail.com>
2024-10-03 18:22:43 +0300

    notification fix

--- microservice-notification/common/src/main/java/com/payment/notification/common/base/BaseResponse.java
+++ microservice-notification/common/src/main/java/com/payment/notification/common/base/BaseResponse.java
@@ -1,6 +1,7 @@
 package com.payment.notification.common.base;
 
 import lombok.*;
+import org.springframework.http.HttpStatus;
 
 import java.io.Serializable;
 
@@ -14,16 +15,18 @@ public class BaseResponse implements Serializable {
 
     private Object data;
     private Integer count;
+    private Integer status;
 
     public static BaseResponse success(Object data) {
-        return BaseResponse.builder().data(data).build();
+        return BaseResponse.builder().data(data).status(HttpStatus.OK.value()).build();
     }
 
     public static BaseResponse success(Object data, Integer count) {
-        return BaseResponse.builder().data(data).count(count).build();
+        return BaseResponse.builder().data(data).status(HttpStatus.OK.value()).count(count).build();
     }
 
     public static BaseResponse error(String message) {
-        return BaseResponse.builder().data(message).build();
+        return BaseResponse.builder().data(message).status(HttpStatus.BAD_REQUEST.value()).build();
     }
+
 }
