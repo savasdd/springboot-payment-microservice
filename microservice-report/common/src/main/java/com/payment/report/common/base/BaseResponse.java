d076327576bca23209e2993ca809dacc56157498
savasdede <svsdd22@gmail.com>
2024-10-03 18:02:06 +0300

    report fix

--- microservice-report/common/src/main/java/com/payment/report/common/base/BaseResponse.java
+++ microservice-report/common/src/main/java/com/payment/report/common/base/BaseResponse.java
@@ -15,4 +15,15 @@ public class BaseResponse implements Serializable {
     private Object data;
     private Integer count;
 
+    public static BaseResponse success(Object data) {
+        return BaseResponse.builder().data(data).build();
+    }
+
+    public static BaseResponse success(Object data, Integer count) {
+        return BaseResponse.builder().data(data).count(count).build();
+    }
+
+    public static BaseResponse error(String message) {
+        return BaseResponse.builder().data(message).build();
+    }
 }
