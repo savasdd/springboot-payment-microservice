b5a0996d831f9a4bf0dd1f174f7101a63caa1f7d
savasdede <svsdd22@gmail.com>
2024-10-13 11:52:48 +0300

    gateway fix

--- microservice-gateway/application/src/main/java/com/payment/gateway/application/GatewayApplication.java
+++ microservice-gateway/application/src/main/java/com/payment/gateway/application/GatewayApplication.java
@@ -3,7 +3,7 @@ package com.payment.gateway.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.payment.gateway")
 public class GatewayApplication {
 
 	public static void main(String[] args) {
---
+++ microservice-gateway/common/src/main/java/com/payment/gateway/common/Test.java
@@ -0,0 +1,4 @@
+package com.payment.gateway.common;
+
+public class Test {
+}
