13e2dc4ee562ab4b8b7bfbea3d45250224c0bd24
savasdede <svsdd22@gmail.com>
2024-10-13 13:08:45 +0300

    gateway fix

---
+++ microservice-gateway/common/src/main/java/com/payment/gateway/common/log/LogInterceptor.java
@@ -0,0 +1,25 @@
+package com.payment.gateway.common.log;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        MDC.put("api", request.getRequestURI());
+        MDC.put("method", request.getMethod());
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        MDC.clear();
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
