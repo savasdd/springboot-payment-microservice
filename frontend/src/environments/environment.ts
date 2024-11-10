6373f097dea32503cc1e64792050090c98d86a07
savasdede <svsdd22@gmail.com>
2024-11-10 16:13:41 +0300

    dev

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8085/api/',
   // apiUrl: 'http://127.0.0.1:8085/api/',
-  applicationName: 'Food api',
+  applicationName: 'PAYMENT',
 };
