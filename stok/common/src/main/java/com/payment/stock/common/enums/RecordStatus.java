7c3f5a9c99f3fcfb59db2734064d19b0bf23c903
savasdede <svsdd22@gmail.com>
2025-01-04 19:06:57 +0300

    fix stock

--- stok/common/src/main/java/com/payment/stock/common/enums/RecordStatus.java
+++ stok/common/src/main/java/com/payment/stock/common/enums/RecordStatus.java
@@ -1,6 +1,15 @@
 package com.payment.stock.common.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum RecordStatus {
-    ACTIVE,
-    DELETED
+    ACTIVE(0),
+    DELETED(1);
+
+    private final Integer code;
+
+    RecordStatus(int code) {
+        this.code = code;
+    }
 }
